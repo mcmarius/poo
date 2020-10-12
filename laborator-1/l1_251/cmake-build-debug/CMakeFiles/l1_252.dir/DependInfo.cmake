8953c3403ccec96e258a19c9865ff3cf03b56387
mcmarius <m.c.marius@outlook.com>
2020-10-12 10:14:26 +0300

    Adăugat folder cu build files

---
+++ laborator-1/l1_251/cmake-build-debug/CMakeFiles/l1_252.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/marius/ClionProjects/l1_252/main.cpp" "C:/Users/marius/ClionProjects/l1_252/cmake-build-debug/CMakeFiles/l1_252.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ laborator-1/l1_251/cmake-build-debug/CMakeFiles/l1_252.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/l1_252.dir/main.cpp.obj"
+  "l1_252.exe"
+  "l1_252.exe.manifest"
+  "l1_252.pdb"
+  "libl1_252.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/l1_252.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
